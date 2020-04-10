e201b65de7f7eefdf9c027cdb868e3cf1fb5e0e1
SAapronova <sapronovaalena@gmail.com>
2020-04-10 21:22:10 +0300

    12

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+WebDriver driver;
+@BeforeTest
+public void setUp(){
+
+
+    WebDriverManager.chromedriver().setup();
+    driver=new ChromeDriver();
+
+}
+
+
+@Test
+public void testOtus(){
+
+    driver.get("https://otus.ru/");
+    System.out.println(driver.getTitle()+"===");
+}
+
+@AfterTest
+    public void quit(){
+
+    if (driver!=null)
+    {
+        driver.quit();
+    }
+
+}
+}
